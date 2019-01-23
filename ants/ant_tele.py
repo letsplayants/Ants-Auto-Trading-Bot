e11a5dd9aaf612f6a6a33dcf483f8d36c7ba3823
lemy0715 <lemy0715@gmail.com>
2019-01-23 08:27:05 -0800

    #15 patch 1

--- ants/ant_tele.py
+++ ants/ant_tele.py
@@ -16,13 +16,17 @@ def __initModule():
         mtoken = readKey('./configs/telegram_bot.key')
         
         try:
+            global use
             use = False
             if(mtoken['use'].upper() == 'TRUE') :
                 use = True
             else :
                 logger.info('Telegram disable')
                 return
-                
+            
+            global token
+            global chat_id
+            global bot
             token = mtoken['bot_token']
             chat_id = mtoken['chat_id']
             bot = telegram.Bot(token=mtoken["bot_token"])
@@ -40,13 +44,21 @@ class AntTelegram:
     def sendMessage(self, msg):
         logger.debug('sendMessage : {}'.format(use))
         if(use) :
-            logger.debug('sendMsg : {}-{}')
+            logger.debug('sendMsg : {}-{}'.format(chat_id, msg))
             bot.sendMessage(chat_id, msg)
         
 
 __initModule()
 
 if __name__ == '__main__':
+    logger = logging.getLogger(__name__)
     logger.setLevel(logging.DEBUG)
+    
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    
+    stream_hander = logging.StreamHandler()
+    stream_hander.setFormatter(formatter)
+    logger.addHandler(stream_hander)
+
     tel = AntTelegram()
-    tel.sendMessage("저는 봇클래스 테스트.")
+    tel.sendMessage("봇클래스 테스트.")
