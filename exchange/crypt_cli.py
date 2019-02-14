e1fd1d3e6417730cc3ea128173db7bd4f726a147
lemy0715 <lemy0715@gmail.com>
2019-02-14 10:28:23 +0900

    API Key 암호화 저장 및 테스트 cli완성

--- exchange/crypt_cli.py
+++ exchange/crypt_cli.py
@@ -9,6 +9,7 @@ import os
 import json
 import base64
 import binascii
+import ccxt
 
 def readKey(filePath):
     if not os.path.isfile(filePath):
@@ -27,7 +28,8 @@ def readKey(filePath):
 def saveConf(filePath, data):
     try:
         with open(filePath, 'w') as fp:
-            json.dump(data, fp)
+            fp.write(json.dumps(data, indent=4))
+            
     except Exception as exp:
         print("Can't load json : {}".format(exp))
         sys.exit(1)
@@ -65,6 +67,7 @@ def decrypt(key_file, msg):
 def add_exchange(config_file, new_exchange):
     #파일을 열어서 기존 설정을 읽어온다
     config = readKey(config_file)
+    new_exchange = new_exchange.upper()
     
     try:
         pub_key = config['key_file']['rsa_pub_file']
@@ -86,33 +89,59 @@ def add_exchange(config_file, new_exchange):
         }
         config[new_exchange] = key_set
         
-        print(config)
         saveConf(config_file, config)
+        print('config file save done')
+        
+        try:
+            test_exchange(config_file, new_exchange)
+        except Exception as texp:
+            print('Did not add new key. try again...')
+            sys.exit(1)
+        
     except Exception as exp:
         print('except : {}'.format(exp))
         sys.exit(1)
     
 def test_exchange(config_file, new_exchange):
-    #파일을 열어서 기존 설정을 읽어온다
+    print('exchange connection test with key')
+    
     config = readKey(config_file)
+    new_exchange = new_exchange.upper()
     
     try:
         pub_key = config['key_file']['rsa_pub_file']
         private_key = config['key_file']['rsa_key_file']
-    
+        
         data = config[new_exchange]['apiKey']
         borg = binascii.a2b_base64(data)
         apiKey = decrypt(private_key, borg)
-        print(apiKey)
         
         data = config[new_exchange]['secret']
         borg = binascii.a2b_base64(data)
         secret = decrypt(private_key, borg)
-        print(secret)
-        
         
         #TODO ccxt를 사용하여 동작테스트를 한다
-        
+        new_exchange = new_exchange.lower()
+        exchange = None
+        for id in ccxt.exchanges:
+            if(id == new_exchange):
+                type_exchange = getattr(ccxt, id)
+                exchange = type_exchange({
+                                'apiKey': apiKey,
+                                'secret': secret,
+                                })
+            
+        if(exchange == None):
+            print('Can''t find exchange {}'.format(new_exchange))
+            return
+
+        try:
+            exchange.fetch_balance()
+        except Exception as texp:
+            print('something is wrong : {}'.format(texp))
+            return
+
+        print('test pass')
     except Exception as exp:
         print('except : {}'.format(exp))
         sys.exit(1)
@@ -135,8 +164,6 @@ if __name__ == "__main__":
     parser.add_argument("-f", "--file", help="config file")
     args = parser.parse_args()
     
-    args.exchange = args.exchange.upper()
-    
     if(args.exchange == 'ALL'):
         print("Can't add exchange name 'ALL'")
         sys.exit(0)
@@ -144,7 +171,7 @@ if __name__ == "__main__":
     if(args.file):
         configFile = args.file
     else:
-        configFile = 'configs/exchanges.conf'
+        configFile = 'configs/exchanges.key'
         
     if(args.cmd == 'add'):
         print("got it", args.cmd)
