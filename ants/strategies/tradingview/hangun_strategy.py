c8731dab8f4e85a7a3fa542b3acc751ea90fb630
lemy0715 <lemy0715@gmail.com>
2019-03-08 00:20:27 +0900

    한군 지표 전략 업데이트, 다중 얼럿트 관리 가능

--- ants/strategies/tradingview/hangun_strategy.py
+++ ants/strategies/tradingview/hangun_strategy.py
@@ -22,20 +22,18 @@ class HanGunStrategy(ants.strategies.strategy.StrategyBase, Observer):
     def __init__(self, args={}):
         self.logger = logging.getLogger(__name__)
         self.data_provider = None
-        self.actionState = 'READY'  #BUY, SELL, READY
-        self.cooldown = 55  #minitue
-        self.actionTime = datetime.now() - timedelta(minutes=self.cooldown)
+        self.states = {}
         self.trader = SmartTrader()
         self.telegram = TelegramRepoter()
         self.db = Sqlite()
         
-        self.upbit = cUpbit({'private_key_file':'configs/ants.conf', 'key_file':'configs/exchanges.key', 'config_file':'configs/upbit.conf', 'telegram': self.telegram, 'db':self.db})
+        self.upbit = cUpbit({'root_config_file':'configs/ants.conf', 'key_file':'configs/exchanges.key', 'config_file':'configs/upbit.conf', 'telegram': self.telegram, 'db':self.db})
         self.trader.add_exchange('UPBIT', self.upbit)
         
-        self.bithumb = cBithumb({'private_key_file':'configs/ants.conf', 'key_file':'configs/exchanges.key', 'config_file':'configs/bithumb.conf', 'telegram': self.telegram, 'db':self.db})
+        self.bithumb = cBithumb({'root_config_file':'configs/ants.conf', 'key_file':'configs/exchanges.key', 'config_file':'configs/bithumb.conf', 'telegram': self.telegram, 'db':self.db})
         self.trader.add_exchange('BITHUMB', self.bithumb)
         
-        self.binance = cBinance({'private_key_file':'configs/ants.conf', 'key_file':'configs/exchanges.key', 'config_file':'configs/binance.conf', 'telegram': self.telegram, 'db':self.db})
+        self.binance = cBinance({'root_config_file':'configs/ants.conf', 'key_file':'configs/exchanges.key', 'config_file':'configs/binance.conf', 'telegram': self.telegram, 'db':self.db})
         self.trader.add_exchange('BINANCE', self.binance)
     
     def run(self):
@@ -60,27 +58,26 @@ class HanGunStrategy(ants.strategies.strategy.StrategyBase, Observer):
     
     def check_signal(self, msg):
         try:
+            exchange = msg['exchange'].upper()
+            coin_name = msg['market'].split('/')[0]
+            market = msg['market'].split('/')[1]
             action = msg['action'].upper()
-        except Exception as ex:
-            self.logger.warning('Can''t get action : {}'.format(ex))
-            return;
-        
-        current = datetime.now()
-        timeGap = self.actionTime + timedelta(minutes=self.cooldown)
-        if(current < timeGap):
-            self.logger.warning('{} signal ignore cause now is cooldown'.format(action))
+        except :
+            self.logger.warning('msg format is wrong : {}'.format(msg))
             return
         
-        if(self.actionState == 'READY'):
+        state = self.get_state(exchange, coin_name, market)
+        
+        if(state == None):
             self.do_action(msg)
-        elif(self.actionState == 'BUY'):
+        elif(state == 'BUY'):
             if(action == 'BUY'):
                 self.do_action(msg, True)
             elif(action == 'SELL'):
                 self.do_action(msg)
-        elif(self.actionState == '2ND_BUY'):
+        elif(state == '2ND_BUY'):
             if(action == 'BUY'):
-                self.logger.warning('This signal({}) will be ignore cause last signal : {}'.format(action, self.actionState))
+                self.logger.warning('This signal({}) will be ignore cause last signal : {}'.format(action, state))
                 return
             elif(action == 'SELL'):
                 self.do_action(msg)
@@ -90,14 +87,14 @@ class HanGunStrategy(ants.strategies.strategy.StrategyBase, Observer):
         print('do action : {}'.format(msg))
         try:
             exchange = msg['exchange'].upper()
-            coinName = msg['market'].split('/')[0]
+            coin_name = msg['market'].split('/')[0]
             market = msg['market'].split('/')[1]
             action = msg['action'].upper()
         except :
             self.logger.warning('msg format is wrong : {}'.format(msg))
             return
         
-        result = self.trader.trading(exchange, market, action, coinName)
+        result = self.trader.trading(exchange, market, action, coin_name)
         if(result == None):
             #트레이딩 실패
             self.logger.warning('Trading was failed')
@@ -109,10 +106,68 @@ class HanGunStrategy(ants.strategies.strategy.StrategyBase, Observer):
             action = '2ND_BUY'
             self.logger.info('2ND BUY done')
             
-        self.actionState = action
-        self.actionTime = datetime.now()
+        self.save_state(exchange, coin_name, market, action)
         
+    def save_state(self, exchange, coin, market, action):
+        record = {
+            'coin' : coin,
+            'market' : market,
+            'exchange' : exchange,
+            'action' : action
+        }
+        """
+        {
+            'upbit': {
+                'xrp': {
+                    'krw': {
+                        'action' : 'buy'
+                    },
+                    'btc' : {
+                        'action' : 'sell'
+                    },
+                    'usdt' : {
+                        'action' : 'buy
+                    }
+                },
+                'trx': {
+                    'krw': {
+                        'action' : 'buy'
+                    },
+                    'btc' : {
+                        'action' : 'sell'
+                    },
+                    'usdt' : {
+                        'action' : 'buy
+                    }
+                }
+            },
+            'bithumb' : {},
+            'binance' : {}
+        }
+        """
 
+        ex_rec = self.__get_dict(self.states, exchange)
+        coin_rec = self.__get_dict(ex_rec, coin)
+        market_rec = self.__get_dict(coin_rec, market)
+        market_rec = {'action' : action}
+
+        coin_rec[market] = market_rec
+        ex_rec[coin] = coin_rec
+        self.states[exchange] = ex_rec
+
+    def __get_dict(self, p, name):
+        r = p.get(name)
+        if(r == None):
+         p[name] = {}
+        
+        return p[name]
+
+    def get_state(self, exchange, coin, market):
+        try:
+            return self.states[exchange][coin][market]['action']
+        except Exception as e:
+            self.logger.debug('{} {}/{} has not states : {}'.format(exchange, coin, market, e))
+            return None
     
 if __name__ == '__main__':
     print('strategy test')
@@ -128,17 +183,35 @@ if __name__ == '__main__':
     logging.getLogger("urllib3.connectionpool").setLevel(logging.WARNING)
     
     st = HanGunStrategy()
-    st.run()
+    # st.run()
     
     # print('SELL Fail-------------------------------------------------------------------')
     # msg = {'market': 'BTC/KRW', 'time': '10M', 'action': 'SELL', 'exchange': 'UPBIT'}
     # st.update(msg)
     
-    print('BUY SELL-------------------------------------------------------------------')
     msg = {'market': 'BTC/KRW', 'time': '10M', 'action': 'BUY', 'exchange': 'UPBIT'}
-    # st.update(msg)
+    st.update(msg)
+    msg = {'market': 'BTC/KRW', 'time': '10M', 'action': 'BUY', 'exchange': 'UPBIT'}
+    st.update(msg)
+    msg = {'market': 'BTC/KRW', 'time': '10M', 'action': 'BUY', 'exchange': 'UPBIT'}
+    st.update(msg)
+    
+    msg = {'market': 'ETH/KRW', 'time': '10M', 'action': 'BUY', 'exchange': 'UPBIT'}
+    st.update(msg)
+    msg = {'market': 'ETH/KRW', 'time': '10M', 'action': 'BUY', 'exchange': 'UPBIT'}
+    st.update(msg)
+    msg = {'market': 'ETH/KRW', 'time': '10M', 'action': 'BUY', 'exchange': 'UPBIT'}
+    st.update(msg)
+    
     msg = {'market': 'BTC/KRW', 'time': '10M', 'action': 'SELL', 'exchange': 'UPBIT'}
-    # st.update(msg)
+    st.update(msg)
+    msg = {'market': 'ETH/KRW', 'time': '10M', 'action': 'SELL', 'exchange': 'UPBIT'}
+    st.update(msg)
+    
+    msg = {'market': 'BTC/KRW', 'time': '10M', 'action': 'SELL', 'exchange': 'UPBIT'}
+    st.update(msg)
+    msg = {'market': 'ETH/KRW', 'time': '10M', 'action': 'SELL', 'exchange': 'UPBIT'}
+    st.update(msg)
     
     # print('BUY BUY BUY SELL-------------------------------------------------------------------')
     # msg = {'market': 'BTC/KRW', 'time': '10M', 'action': 'BUY', 'exchange': 'UPBIT'}
