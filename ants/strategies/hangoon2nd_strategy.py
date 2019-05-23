c1f4b7f95a26c582e7e24b12039826b1a3b1b141
lemy0715 <lemy0715@gmail.com>
2019-05-23 16:03:36 +0000

    공지방에 픽 나가는 종목에 대해 누적 수익률 표시

--- ants/strategies/hangoon2nd_strategy.py
+++ ants/strategies/hangoon2nd_strategy.py
@@ -10,6 +10,8 @@ from ants.provider.email_provider import EmailProvider
 from messenger.q_publisher import MQPublisher
 from env_server import Enviroments
 
+from exchangem.exchanges.upbit import Upbit as cUpbit
+
 
 class Mail2QuickTradingStrategy(ants.strategies.strategy.StrategyBase, Observer):
     """
@@ -21,6 +23,14 @@ class Mail2QuickTradingStrategy(ants.strategies.strategy.StrategyBase, Observer)
         self.telegram_messenger_exchange_name = Enviroments().qsystem.get_telegram_messenge_q()
         self.messenger_q = MQPublisher(self.telegram_messenger_exchange_name)
         self.logger.debug('telegram message q name : {}'.format(self.telegram_messenger_exchange_name))
+        self.exchanges = {}
+        self.add_exchange('upbit', cUpbit())
+
+    def add_exchange(self, name, exchange):
+        self.exchanges[name.upper()] = exchange
+        
+    def get_exchange(self, name):
+        return self.exchanges[name.upper()]
     
     def run(self):
         #전략에서 사용할 데이터 제공자를 등록 후 실행
@@ -39,7 +49,9 @@ class Mail2QuickTradingStrategy(ants.strategies.strategy.StrategyBase, Observer)
         
         msg=msg[msg.find(':')+2:]
         
-        if(self.check_signal(msg)):
+        do, add_msg = self.check_signal(msg)
+        if(do):
+            msg = msg + '\n' + add_msg
             self.messenger_q.send('{}'.format(msg))
         
         pass
@@ -49,6 +61,14 @@ class Mail2QuickTradingStrategy(ants.strategies.strategy.StrategyBase, Observer)
         self.data_provider.stop()
         self.logger.info('Strategy will stop')
     
+    def get_avg_price(self):
+        text = msg.split(' ')
+        
+        command = text[0].strip().upper()
+        exchange_name = text[1].strip().upper()
+        market = text[2].strip().upper()
+        coin_name = text[3].strip().upper()
+    
     def check_signal(self, msg):
         """
         세번째 buy일 경우 false
@@ -57,24 +77,84 @@ class Mail2QuickTradingStrategy(ants.strategies.strategy.StrategyBase, Observer)
         text = msg.split(' ')
         
         command = text[0].strip().upper()
-        exchange = text[1].strip().upper()
+        exchange_name = text[1].strip().upper()
         market = text[2].strip().upper()
         coin_name = text[3].strip().upper()
         
+        symbol = '{}/{}'.format(coin_name, market)
+        exchange = self.get_exchange(exchange_name)
+        if(exchange is None):
+            return False, '{} 이름이 유효하지 않습니다.'.format(exchange_name)
+        
+        #항목 아래 함수가 자주 호출되면 block 걸린다
+        price = exchange.get_last_price(symbol)
+        
         if(command == 'BUY'):
-            buy_cnt = self.get_state(exchange, coin_name, market)
+            buy_info = self.get_state(exchange_name, coin_name, market)
+            print('buy info : {}'.format(buy_info))
+            try:
+                buy_cnt = int(buy_info['buy_cnt'])
+            except :
+                buy_cnt = 0
+            
+            if(buy_cnt >= 2):
+                b0 = float(buy_info['1'])
+                b1 = float(buy_info['2'])
+                return False, '{}\n평균 구매 단가: {}\n현재 2회 구매 중입니다\n추가 매입하지 않음\n'.format(coin_name, ((b0 + b1) / 2))
+            
+            if(buy_cnt == 0):
+                price_msg = '1회차 구매 단가: {}'.format(price)
+            elif(buy_cnt == 1):
+                b0 = float(buy_info['1'])
+                price_msg = '1회차 구매 단가: {}\n'.format(b0)
+                price_msg += '2회차 구매 단가: {}\n'.format(price)
+                price_msg += '평균 구매 단가: {}\n'.format((b0 + price) / 2)
+            
             buy_cnt += 1
-            if(buy_cnt > 2):
-                return False
+            accumulate_profit = None
+                
         elif(command == 'SELL'):
+            buy_info = self.get_state(exchange_name, coin_name, market)
+            try:
+                buy_cnt = int(buy_info['buy_cnt'])
+            except :
+                buy_cnt = 0
+            
+            if(buy_cnt == 0):
+                buy_price = price #구매한적 없음
+            elif(buy_cnt == 1):
+                buy_price = float(buy_info['1'])
+            elif(buy_cnt == 2):
+                b0 = float(buy_info['1'])
+                b1 = float(buy_info['2'])
+                buy_price = (b0 + b1) / 2
+            
+            try:
+                accumulate_profit = buy_info['accumulate']
+            except:
+                accumulate_profit = 0
+            
+            sell_price = price
+            profit_price = ((sell_price - buy_price)  * 100) / buy_price
+            accumulate_profit = (accumulate_profit + profit_price) / 2
+            
+            price_msg = '매매 수익률 : {}%\n'.format(profit_price)
+            price_msg += '매수 단가 : {}\n'.format(buy_price)
+            price_msg += '매도 단가 : {}\n'.format(sell_price)
+            price_msg += '누적 수익률 : {}%\n'.format(profit_price)
+            
             buy_cnt = 0
+            
+        self.save_state(exchange_name, coin_name, market, 'buy', buy_cnt, price, accumulate_profit)
         
-        self.save_state(exchange, coin_name, market, 'buy', buy_cnt)
-        
-        return True
+        return True, price_msg
    
-    def save_state(self, exchange, coin, market, action, buy_cnt):
+    def save_state(self, exchange, coin, market, action, buy_cnt, price, accumulate=None):
         class_name = self.__class__.__name__.lower()
+        exchange = exchange.lower()
+        coin = coin.lower()
+        market = market.lower()
+        action = action.lower()
         
         # 기존 데이터를 읽어서 거기에 애드한다
         record_strategy = self.__get_dict__(Enviroments().strategies, class_name)
@@ -82,6 +162,10 @@ class Mail2QuickTradingStrategy(ants.strategies.strategy.StrategyBase, Observer)
         record_coin = self.__get_dict__(record_exchange, coin)
         record_market = self.__get_dict__(record_coin, market)
         
+        if(accumulate != None):
+            record_market['accumulate'] = accumulate
+        if(buy_cnt != 0):
+            record_market[str(buy_cnt)] = price
         record_market['buy_cnt'] = buy_cnt
         record_coin[market] = record_market
         record_exchange[coin] = record_coin
@@ -98,19 +182,24 @@ class Mail2QuickTradingStrategy(ants.strategies.strategy.StrategyBase, Observer)
         return p[name]
 
     def get_state(self, exchange, coin, market):
+        exchange = exchange.lower()
+        coin = coin.lower()
+        market = market.lower()
+        
         try:
             class_name = self.__class__.__name__.lower()
             cnt = Enviroments().strategies[class_name][exchange][coin][market]
-            return cnt['buy_cnt']
+            return cnt
         except Exception as e:
             self.logger.debug('{} {}/{} has not states : {}'.format(exchange, coin, market, e))
-            return 0
+            return None
     
 if __name__ == '__main__':
     print('strategy test')
     
     import os
-    path = os.path.dirname(__file__) + '../../configs/ant_auto.conf'
+    path = os.path.dirname(__file__) + '/../../configs/ant_auto.conf'
+    print('path : {}'.format(path))
     Enviroments().load_config(path)
     
     logger = logging.getLogger()
@@ -123,6 +212,8 @@ if __name__ == '__main__':
     logging.getLogger("ccxt.base.exchange").setLevel(logging.WARNING)
     logging.getLogger("urllib3.connectionpool").setLevel(logging.WARNING)
     logging.getLogger("telegram").setLevel(logging.WARNING)
+    logging.getLogger("exchangem").setLevel(logging.ERROR)
+    logging.getLogger("exchange").setLevel(logging.ERROR)
     
     
     s='TradingView Alert: sell upbit krw btc 0% 100%'
@@ -137,67 +228,93 @@ if __name__ == '__main__':
     # test.save_state('binance','xrp','btc','buy',1)
     # test.save_state('bithumb','btc','krw','buy',1)
     
-    # state = test.get_state('upbit', 'eth', 'krw')
-    # print(state)
-    #------------------------------------------
-    print('2 buy test')
-    msg ='buy upbit krw btc 0% 100%'
-    if(test.check_signal(msg)):
-        print('1 do buy')
+    # print('0번 구매 테스트')
+    # msg ='sell upbit krw btc 0% 100%'
+    # do, add_msg = test.check_signal(msg)
+    # if(do):
+    #     print('1 do sell : {}'.format(add_msg))
     
-    msg ='buy upbit krw btc 0% 100%'
-    if(test.check_signal(msg)):
-        print('2 do buy')
+    # print('한번 구매 테스트')
+    # msg ='buy upbit krw btc 0% 100%'
+    # do, add_msg = test.check_signal(msg)
+    # if(do):
+    #     print('1 do buy : {}'.format(add_msg))
+    
+    # msg ='sell upbit krw btc 0% 100%'
+    # do, add_msg = test.check_signal(msg)
+    # if(do):
+    #     print('1 do sell : {}'.format(add_msg))
+    
+    # print('두번 구매 테스트')
+    # msg ='buy upbit krw btc 0% 100%'
+    # do, add_msg = test.check_signal(msg)
+    # if(do):
+    #     print('1 do buy : {}\n\n'.format(add_msg))
+    
+    # msg ='buy upbit krw btc 0% 100%'
+    # do, add_msg = test.check_signal(msg)
+    # if(do):
+    #     print('2 do buy : {}\n\n'.format(add_msg))
+    
+    # msg ='sell upbit krw btc 0% 100%'
+    # do, add_msg = test.check_signal(msg)
+    # if(do):
+    #     print('1 do sell : {}'.format(add_msg))
+    
+    # print('세번 구매 테스트')
+    # msg ='buy upbit krw btc 0% 100%'
+    # do, add_msg = test.check_signal(msg)
+    # if(do):
+    #     print('1 do buy : {}\n\n'.format(add_msg))
+    
+    # msg ='buy upbit krw btc 0% 100%'
+    # do, add_msg = test.check_signal(msg)
+    # if(do):
+    #     print('2 do buy : {}\n\n'.format(add_msg))
+    # msg ='buy upbit krw btc 0% 100%'
+    # do, add_msg = test.check_signal(msg)
+    # if(not do):
+    #     print('3 do not buy : {}\n\n'.format(add_msg))
+    
+    # msg ='sell upbit krw btc 0% 100%'
+    # do, add_msg = test.check_signal(msg)
+    # if(do):
+    #     print('1 do sell : {}'.format(add_msg))
+    
+    
+    # #------------------------------------------
+    # print('complex buy test')
+    # msg ='buy upbit krw btc 0% 100%'
+    # if(test.check_signal(msg)):
+    #     print('BTC 1 do buy')
+    
+    # msg ='buy upbit krw eth 0% 100%'
+    # if(test.check_signal(msg)):
+    #     print('ETH 1 do buy')
         
-    msg ='buy upbit krw btc 0% 100%'
-    if(test.check_signal(msg)):
-        print('3 do buy')
+    # msg ='buy upbit krw btc 0% 100%'
+    # if(test.check_signal(msg)):
+    #     print('BTC 2 do buy')
     
-    msg ='sell upbit krw btc 0% 100%'
-    if(test.check_signal(msg)):
-        print('1 do sell')
+    # msg ='buy upbit krw btc 0% 100%'
+    # if(test.check_signal(msg)):
+    #     print('BTC 3 do buy')
+    
+    # msg ='sell upbit krw btc 0% 100%'
+    # if(test.check_signal(msg)):
+    #     print('BTC 1 do sell')
         
-    msg ='buy upbit krw btc 0% 100%'
-    if(test.check_signal(msg)):
-        print('1 do buy')
+    # msg ='buy upbit krw eth 0% 100%'
+    # if(test.check_signal(msg)):
+    #     print('eth 2 do buy')
     
-    msg ='sell upbit krw btc 0% 100%'
-    if(test.check_signal(msg)):
-        print('1 do sell')
-    
-    #------------------------------------------
-    print('complex buy test')
-    msg ='buy upbit krw btc 0% 100%'
-    if(test.check_signal(msg)):
-        print('BTC 1 do buy')
-    
-    msg ='buy upbit krw eth 0% 100%'
-    if(test.check_signal(msg)):
-        print('ETH 1 do buy')
+    # msg ='buy upbit krw eth 0% 100%'
+    # if(test.check_signal(msg)):
+    #     print('eth 3 do buy')
         
-    msg ='buy upbit krw btc 0% 100%'
-    if(test.check_signal(msg)):
-        print('BTC 2 do buy')
-    
-    msg ='buy upbit krw btc 0% 100%'
-    if(test.check_signal(msg)):
-        print('BTC 3 do buy')
-    
-    msg ='sell upbit krw btc 0% 100%'
-    if(test.check_signal(msg)):
-        print('BTC 1 do sell')
-        
-    msg ='buy upbit krw eth 0% 100%'
-    if(test.check_signal(msg)):
-        print('eth 2 do buy')
-    
-    msg ='buy upbit krw eth 0% 100%'
-    if(test.check_signal(msg)):
-        print('eth 3 do buy')
-        
-    msg ='sell upbit krw eth 0% 100%'
-    if(test.check_signal(msg)):
-        print('eth 1 do sell')
+    # msg ='sell upbit krw eth 0% 100%'
+    # if(test.check_signal(msg)):
+    #     print('eth 1 do sell')
     
     
     
