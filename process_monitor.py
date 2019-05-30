903a1a9ffc5f5424d982c4f4ce0ca0132618138d
lemy0715 <lemy0715@gmail.com>
2019-05-30 06:25:00 +0000

    라즈베리파이에서 업그레이드 실패하는 문제 수정

--- process_monitor.py
+++ process_monitor.py
@@ -7,14 +7,17 @@ from subprocess import Popen, PIPE
 current_pwd = os.getcwd() + '/'
 main_py = current_pwd + 'ants/main.py'
 
+command = 'python3'
 su = ''
 if(current_pwd.find('/home/pi/') == 0):
     #라즈베리파이로 인식한다
-    su = 'sudo '
-    
+    #라즈베리파이의 경우 python3으로 구동하면 시스템 기본인 3.5 버젼으로 구동된다
+    #최초 OS 설정 때 3.6버젼을 python으로 맵핑해 둔 상황이다.
+    #그러므로 python으로 구동한다
+    command = 'python'
+
 while(True):
     try:
-        command = '{}python3'.format(su)
         proc = Popen([command, main_py], stdout=PIPE, universal_newlines=True)
     except Exception as exp:
         print('Can''t start {} with exception : {}'.format(main_py, exp))
